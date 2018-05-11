210f3c5267f8209e81558017c0844ad57aa4f743
theguyfromtv <shaharblonde@The-Tesseract.local>
2018-05-11 14:50:51 -0400

    some basic game assets working

--- Public/js/sceneMain.js
+++ Public/js/sceneMain.js
@@ -1,63 +1,113 @@
-class SceneMain extends Phaser.Scene {
-    constructor() {
-        super('SceneMain');
+//basic config object sets the rules for the whole game
+
+var config = {
+
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
-    preload() {
-        this.load.image('background1_clouds_1', '../assets/background/background1_clouds_1.png');
-        this.load.image('background1_clouds_2', '../assets/background/background1_clouds_2.png');
-        this.load.image('background1_clouds_3', '../assets/background/background1_clouds_3.png');
-        this.load.image('background1_clouds_4', '../assets/background/background1_clouds_4.png');
-        this.load.image('background1_rocks_1', '../assets/background/background1_rocks_1.png');
-        this.load.image('background1_rocks_2', '../assets/background/background1_rocks_2.png');
-        this.load.image('background1_sky', '../assets/background/background1_sky.png');
-    }
+    };
+//global background object
+let background={};
 
-    create() {
+//global character object
+let roxy={};
 
-        // Get the window sizes
-        let windowWidth = window.innerWidth;
-        let windowHeight = window.innerHeight;
+//global ground object
+let ground={}
 
-        // Find the center of the top space
-        let topBackgroundXOrigin = windowWidth / 2;
-        let topBackgroundYOrigin = (windowHeight / 5) * 1.5;
-        let topBackgroundHeight = (windowHeight / 5) * 3;
-        
-        // Base width and height of the images
-        let imageBaseWidth = 1920;
-        let imageBaseHeight = 1080;
-        let heightRatio = topBackgroundHeight / imageBaseHeight;
+let frameNames;
 
-        // Add the sky image at the right location and resize it to take all the space, no scaling needed
-        let skyImage = this.add.image(topBackgroundXOrigin, topBackgroundYOrigin, 'background1_sky');
-        skyImage.setDisplaySize(windowWidth, topBackgroundHeight);
+//constructor creates the game!
+var game = new Phaser.Game(config);   
 
-        // Add each layer one by one
-        this.cloud1 = this.add.tileSprite(topBackgroundXOrigin, topBackgroundYOrigin, imageBaseWidth, imageBaseHeight, 'background1_clouds_1');
-        this.cloud1.setScale(heightRatio);
+    function preload() {
 
-        this.cloud2 = this.add.tileSprite(topBackgroundXOrigin, topBackgroundYOrigin, imageBaseWidth, imageBaseHeight, 'background1_clouds_2');
-        this.cloud2.setScale(heightRatio);
-        
-        this.rocks1 = this.add.tileSprite(topBackgroundXOrigin, topBackgroundYOrigin, imageBaseWidth, imageBaseHeight, 'background1_rocks_1');
-        this.rocks1.setScale(heightRatio);
-        
-        this.cloud3 = this.add.tileSprite(topBackgroundXOrigin, topBackgroundYOrigin, imageBaseWidth, imageBaseHeight, 'background1_clouds_3');
-        this.cloud3.setScale(heightRatio);
-        
-        this.rocks2 = this.add.tileSprite(topBackgroundXOrigin, topBackgroundYOrigin, imageBaseWidth, imageBaseHeight, 'background1_rocks_2');
-        this.rocks2.setScale(heightRatio);
+        ///loading the background 
+        this.load.image('background', '../public/assets/background/seamless-pattern-1.jpg');
+        //loading ground image
+        this.load.image("ground", "../public/assets/enviroment/metal-construction-barriers/metal_plate_shiny_pattern.png");
+        //loading the main protagonist and the JSON attached to her with all the info about the spritesheet
+        this.load.atlas('roxy', '../public/assets/sprites/Roxy.png', '../public/assets/sprites/Roxy.json');
 
-        this.cloud4 = this.add.tileSprite(topBackgroundXOrigin, topBackgroundYOrigin, imageBaseWidth, imageBaseHeight, 'background1_clouds_4');
-        this.cloud4.setScale(heightRatio);
+
+
     }
 
-    update() {
-        this.cloud1.tilePositionX += 0.05;
-        this.cloud2.tilePositionX += 0.05;
-        this.rocks1.tilePositionX += 0.10;
-        this.cloud3.tilePositionX += 0.15;
-        this.rocks2.tilePositionX += 0.20;
-        this.cloud4.tilePositionX += 0.30;
+    function create() {
+        //placing the background as a repeating tile
+        background = this.add.tileSprite(0, -30, config.width, config.height, "background").setOrigin(0);
+
+        console.log(config.height)
+
+        ground = this.add.tileSprite(0, 630, 50,50,"ground").setOrigin(0);
+
+
+        //adding in main character
+        roxy=this.physics.add.sprite(100, 450, 'roxy')
+        
+        //giving her some physics(bounce and making sure she doesn't go off screen)
+        roxy.setBounce(0.2);
+        roxy.setCollideWorldBounds(true);
+
+        this.physics.add.collider(roxy, ground);
+
+        //getting main player's running animation frames
+
+        frameNames = this.anims.generateFrameNames("roxy", {
+            start: 0, end: 17, zeroPad: 3,
+            prefix: 'Running_', suffix: '.png'
+        });
+
+        console.log(frameNames)
+
+        //running animation creator
+
+        this.anims.create({
+            key: 'run',
+            frames: frameNames,
+            frameRate: 30,
+            repeat: -1
+        });
+
+        //calling on the animation
+        roxy.anims.play('run');
+
+
     }
-}
+
+        
+    
+
+    
+
+    function update() {
+
+    
+        
+
+
+    
+        //making the background scroll automatically 
+        background.tilePositionX += 8;
+        //ground.tilePositionXX+=8
+    }
+
+
+
+
+
+    
+
+  
