7413d13d66662b427ba121636aaa1c7c7d21d7e8
TheguyfromTV <goldfingerorama@gmail.com>
2018-05-15 02:56:05 -0400

    small fix

--- public/js/tempo.js
+++ public/js/tempo.js
@@ -1,7 +1,6 @@
 let result2;
 let beatArr;
 
-
 $(document).ready(function(err){
     $.ajax("/upload", {
         type: "GET",
@@ -10,130 +9,60 @@ $(document).ready(function(err){
             console.log(results);
             let songPath2=results
 
-            var accessId = 'ca88977b-8027-4906-b6c6-8c2a10948caf';
-            var taskUrl = 'analyze/melody';
-            var parameters = { blocking: false, format: 'json', access_id: accessId };
+                var accessId = 'ca88977b-8027-4906-b6c6-8c2a10948caf';
+                var taskUrl = 'analyze/tempo';
+                var parameters = { blocking: false, format: 'json', access_id: accessId };
 
-            let songLink2= 'https://protected-savannah-39174.herokuapp.com/'+songPath2;
-            //changing the API paramaters and adding in the song (hardcoded for now).
-            parameters['detailed_result'] = 'false';
-            parameters['input_file'] = songLink2;
 
-                
-            function onTaskStarted(data) {
-                var fileId = data.file.file_id;
-                
-                // request task progress every 500ms
-                var polling = setInterval(pollTaskProgress, 500);
-            
-                function pollTaskProgress() {
-                    $.ajax({ url: 'https://api.sonicAPI.com/file/status?file_id=' + fileId + '&access_id=' + accessId + '&format=json', 
-                            crossDomain: true, success: function(data) {
-                        if (data.file.status == 'ready') {
-                            onTaskSucceeded(fileId);
-                            clearInterval(polling);
-                        } else if (data.file.status == 'working') {
-                            $('#result').text(data.file.progress + '% done');
-                        }
-                    }});
-                }
-            }
-
-            function onTaskSucceeded(fileId) {
-                var downloadUrl = 'https://api.sonicAPI.com/file/download?file_id=' + fileId + '&access_id=' + accessId + '&format=json';
-                
-                $.ajax({ url: downloadUrl, crossDomain: true, success: function(data) {
-                    //doing some math to figure out the range for the game- finding the average note pitch and the highest and lowest pitches, as well as mid points between highest and average and lowest and average. This will result in five numbers-lowest pitch, low midpoint, average, high midpoint, highest pitch, also finding the average volume of notes in the track.
-                    result=data
-                    noteArr=data.melody_result.notes
-                    console.log('success')
+                let songLink2='https://protected-savannah-39174.herokuapp.com/'+songPath2;
+                // the values for these parameters were taken from the corresponding controls in the demo form
+                parameters['input_file'] = songLink;
                     
-                    for (let i = 0; i<noteArr.length;i++){
-                        noteAvg+=noteArr[i].midi_pitch;
-                        notePitchArr.push(noteArr[i].midi_pitch)
-                        avgVol+=noteArr[i].volume;
-                        
+                function onTaskStarted(data) {
+                    var fileId = data.file.file_id;
+                    
+                    // request task progress every 500ms
+                    var polling = setInterval(pollTaskProgress, 500);
+                
+                    function pollTaskProgress() {
+                        $.ajax({ url: 'https://api.sonicAPI.com/file/status?file_id=' + fileId + '&access_id=' + accessId + '&format=json', 
+                                crossDomain: true, success: function(data) {
+                            if (data.file.status == 'ready') {
+                                onTaskSucceeded(fileId);
+                                clearInterval(polling);
+                            } else if (data.file.status == 'working') {
+                                $('#result').text(data.file.progress + '% done');
+                            }
+                        }});
                     }
-                    noteHigh=Math.max.apply(null, notePitchArr)
-                    noteLow=Math.min.apply(null, notePitchArr)
-                    avgVol/=noteArr.length;
-                    noteAvg/=noteArr.length;
-                    highMid=(noteAvg+noteHigh)/2
-                    lowMid=(noteAvg+noteLow)/2
+                }
 
-                    $("body").append("<button id='gameStart'>start game</button>")
+                function onTaskSucceeded(fileId) {
+                    var downloadUrl = 'https://api.sonicAPI.com/file/download?file_id=' + fileId + '&access_id=' + accessId + '&format=json';
+                    
+                    $.ajax({ url: downloadUrl, crossDomain: true, success: function(data) {
+                        console.log(data)
+                        result2=data
+                        beatArr=data.auftakt_result.click_marks;
+                        console.log(beatArr)
 
-                }});
-            }
-
-            function onTaskFailed(response) {
-                var data = $.parseJSON(response.responseText);
-                var errorMessages = data.errors.map(function(error) { return error.message; });
-            
-                $('#result').text('Task failed, reason: ' + errorMessages.join(','));
-            }
-
-            // start task when clicking on the "Start task" button
-            $(document).ready(function() {
-                $(document).on("click","#start", function() {
-                    console.log(songLink)
-                    // execute an HTTP GET using the task's URL, the parameters and callback functions defined above
-                    $.ajax({ url: 'https://api.sonicAPI.com/' + taskUrl, data: parameters, 
-                            success: onTaskStarted, error: onTaskFailed, crossDomain: true });
-                });
-            });
-            var accessId = 'ca88977b-8027-4906-b6c6-8c2a10948caf';
-            var taskUrl = 'analyze/tempo';
-            var parameters = { blocking: false, format: 'json', access_id: accessId };
-
-            // the values for these parameters were taken from the corresponding controls in the demo form
-            parameters['input_file'] = 'https://protected-savannah-39174.herokuapp.com/'+songPath;
-                
-            function onTaskStarted(data) {
-                var fileId = data.file.file_id;
-                
-                // request task progress every 500ms
-                var polling = setInterval(pollTaskProgress, 500);
-            
-                function pollTaskProgress() {
-                    $.ajax({ url: 'https://api.sonicAPI.com/file/status?file_id=' + fileId + '&access_id=' + accessId + '&format=json', 
-                            crossDomain: true, success: function(data) {
-                        if (data.file.status == 'ready') {
-                            onTaskSucceeded(fileId);
-                            clearInterval(polling);
-                        } else if (data.file.status == 'working') {
-                            $('#result').text(data.file.progress + '% done');
-                        }
                     }});
                 }
-            }
 
-            function onTaskSucceeded(fileId) {
-                var downloadUrl = 'https://api.sonicAPI.com/file/download?file_id=' + fileId + '&access_id=' + accessId + '&format=json';
+                function onTaskFailed(response) {
+                    var data = $.parseJSON(response.responseText);
+                    var errorMessages = data.errors.map(function(error) { return error.message; });
                 
-                $.ajax({ url: downloadUrl, crossDomain: true, success: function(data) {
-                    console.log(data)
-                    result2=data
-                    beatArr=data.auftakt_result.click_marks;
-                    console.log(beatArr)
+                    $('#result').text('Task failed, reason: ' + errorMessages.join(','));
+                }
 
-                }});
-            }
-
-            function onTaskFailed(response) {
-                var data = $.parseJSON(response.responseText);
-                var errorMessages = data.errors.map(function(error) { return error.message; });
-            
-                $('#result').text('Task failed, reason: ' + errorMessages.join(','));
-            }
-
-            // start task when clicking on the "Start task" button
-            $(document).ready(function() {
-                $('#start').click(function() {
-                    // execute an HTTP GET using the task's URL, the parameters and callback functions defined above
-                    $.ajax({ url: 'https://api.sonicAPI.com/' + taskUrl, data: parameters, 
-                            success: onTaskStarted, error: onTaskFailed, crossDomain: true });
+                // start task when clicking on the "Start task" button
+                $(document).ready(function() {
+                    $('#start').click(function() {
+                        // execute an HTTP GET using the task's URL, the parameters and callback functions defined above
+                        $.ajax({ url: 'https://api.sonicAPI.com/' + taskUrl, data: parameters, 
+                                success: onTaskStarted, error: onTaskFailed, crossDomain: true });
+                    });
                 });
-            });
-    })
+});
 })
