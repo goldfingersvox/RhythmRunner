d7c0d6a229aaaf18939f0eccfc2d804c47d567ae
theguyfromtv <goldfingerorama@gmail.com>
2018-06-04 18:07:59 -0400

    fixin

--- public/js/sceneMain.js
+++ public/js/sceneMain.js
@@ -125,8 +125,6 @@ let gameScene = {
 
         //adding in the checker that will know if we miss one
         checker=this.physics.add.sprite(-75,390, 'checker')
-
-        grounds=this.physics.add.staticGroup()
         
         grounds.create(550,860, 'ground')
 
